ae5f23715e698267cf388182c7c79d9ad7139442
Kovács Máté <kovacsm22@ADY-NAGYATAD.SULINET.HU>
2023-09-12 09:40:38 +0200

    Órai 2023.09.12

---
+++ órai/valtozok.py
@@ -0,0 +1,28 @@
+#változók próbálgatása
+
+szam = 42
+print(szam)
+
+valosSzam = 2.3
+print(valosSzam)
+
+szoveg = 'cigany'
+print(szoveg)
+
+sutANap = True
+print(sutANap)
+
+szam = szam + 2
+print(szam)
+szam = szam + 2
+print(szam)
+
+print(szam-10)
+print(szam-10)
+
+#maradékos osztás (%)
+szam = 12
+print(szam%12)
+#egész osztás
+print(szam//10)
+print(szam//13)
