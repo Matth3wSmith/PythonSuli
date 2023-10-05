560615ce65c84a3cda7ed155563d9efe92a14ec6
Máté <komate1011@gmail.com>
2023-10-05 22:55:39 +0200

    Gyak

--- órai/gyakorlas0920.py
+++ órai/gyakorlas0920.py
@@ -1,7 +1,7 @@
 # Kétjegyű számok kiiratása ciklussal
 x = 10
 for i in range(1,101):
-    print(i, end="  ")
+    print(i, end=" ")
 
 if i % 10 == 0:
     print(" ")
--- órai/py0922.py
+++ órai/py0922.py
@@ -18,8 +18,34 @@ for b in range(7, 101, 7):
     print(b)
 else:
     print("\n Kész!")
-#Irassátok ki az 500 és 2000 között azokat a számokat ahol a tízesek helyén 0 van.
+#Irassátok ki az 500 és 2000 között azokat a számokat ahol a tízesek helyén 0 van
+for c in range(501, 2000):
+    if (c-1)%100==0 or (c-2)%100==0 or (c-3)%100==0 or (c-4)%100==0 or (c-5)%100==0 or (c-6)%100==0 or (c-7)%100==0 or (c-8)%100==0 or (c-9)%100==0:
+       print(c, end=' ');
+else:
+    print("Kész!")
 
 #Irassátok ki azokat a négyzetszámokat, amelyek 3 számjegyűek
-
+for d in range(1, 50):
+    if len(str(d*d))==3:
+        print(d);
+else:
+    print("Kész!!")
+    
 #Számold meg, mennyi 3 jegyű négyzetszám van
+#for d in range(1, 50):
+#    if len(str(d*d))==3:
+#        negyzetSzam = d*d
+#        lista = []
+#        lista.append(negyzetSzam)
+#        hossz = len(lista)
+#        print(negyzetSzam)
+#print(hossz)
+
+for d in range(1, 50):
+    if len(str(d*d))==3:
+        print(d);
+        szamok = []
+        szamok.append(d)
+else:
+    print(len(szamok))
