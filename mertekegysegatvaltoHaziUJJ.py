296d770d2a59c5859765024fb157bb6f76821382
Máté <komate1011@gmail.com>
2023-10-08 21:34:31 +0200

    Űrmérték + Térfogat 2.1

--- mertekegysegatvaltoHaziUJJ.py
+++ mertekegysegatvaltoHaziUJJ.py
@@ -70,7 +70,7 @@ while tipusId!=0 or egysegId!=0 or egysegId2!=0:
     egysegId="";
 
     #Mértékegység  bekérése (Miből)
-    #ŰRMÉRTÉK+TÉRFOGAT
+#ŰRMÉRTÉK+TÉRFOGAT
     #print(len(urmertek+terfogat)+1)
     if tipusId==5:
         while egysegId=="" or egysegId not in range(len(terfogat+urmertek)+1):
@@ -95,21 +95,22 @@ while tipusId!=0 or egysegId!=0 or egysegId2!=0:
     
     print("#"*40)
     egysegId-=1
+    #ŰRMÉRTÉK+TÉRFOGAT mértékegység kiiratása
     if tipusId==5:
         terfogatUr=[]
         for e,elem in enumerate(terfogat):
-            print("\t",str(e+1)+":",elem)
+            #print("\t",str(e+1)+":",elem)
             terfogatUr.append(elem)
         for b,elem2 in enumerate(urmertek):
-            print("\t",str(b+6)+":",elem2)
+            #print("\t",str(b+6)+":",elem2)
             terfogatUr.append(elem2)
         print("Kívánt mértékegységből való átváltás:", terfogatUr[egysegId])
     else: 
         print("Kívánt mértékegységből való átváltás:", egysegek[tipusId][egysegId])
-    print(terfogatUr)
+    #print(terfogatUr)
 
     #Mértékegység kiírása (Mibe)
-                                #ŰRMÉRTÉK+TÉRFOGAT
+#ŰRMÉRTÉK+TÉRFOGAT
     if tipusId==5:
         
         for e,elem in enumerate(terfogat):
@@ -126,21 +127,34 @@ while tipusId!=0 or egysegId!=0 or egysegId2!=0:
     egysegId2="";
 
     #Mértékegység bekérése (Mibe)
-    while egysegId2=="" or egysegId not in range(len(egysegek[tipusId])+1):
-        try:
-            egysegId2 = int(input("Válaszd ki, hogy mibe szeretnél átváltani!:"));
-            if  egysegId2 not in range(len(egysegek[tipusId])+1):
-                raise
-        except:
-            print("Nem jó! Válassz a listából!")
+#ŰRMÉRTÉK+TÉRFOGAT
+    if tipusId==5:
+        while egysegId2=="" or egysegId2 not in range(len(terfogat+urmertek)+1):
+            try:
+                egysegId2 = int(input("Válaszd ki, hogy mibe szeretnél átváltani!:"));
+                if  egysegId2 not in range(len(terfogat+urmertek)+1):
+                    raise
+            except:
+                print("Nem jó! Válassz a listából!")
+    else:
+        while egysegId2=="" or egysegId2 not in range(len(egysegek[tipusId])+1):
+            try:
+                egysegId2 = int(input("Válaszd ki, hogy mibe szeretnél átváltani!:"));
+                if  egysegId2 not in range(len(egysegek[tipusId])+1):
+                    raise
+            except:
+                print("Nem jó! Válassz a listából!")
             
     #Ha a kilépést(0) választják akkor véget ér a program
     if egysegId2==0:
         break
-
+#ŰRMÉRTÉK+TÉRFOGAT mértékegység kiiratása
     egysegId2-=1
     print("#"*40)
-    print("Kívánt mértékegységbe váltás:", egysegek[tipusId][egysegId2])
+    if tipusId==5:
+        print("Kívánt mértékegységből való átváltás:", terfogatUr[egysegId2])
+    else: 
+        print("Kívánt mértékegységből való átváltás:", egysegek[tipusId][egysegId2])
     print("#"*40)
 
 
@@ -153,21 +167,78 @@ while tipusId!=0 or egysegId!=0 or egysegId2!=0:
         except:
             print("Nem jó! Számot írj be!")
     szam=float(szam)
-    if egysegId<egysegId2:
-        #print(valtok[tipusId][egysegId:egysegId2])
-        szorzo = 1
-        for e in valtok[tipusId][egysegId:egysegId2]:
-            szorzo *= e
-        eredmeny = szam/szorzo
-    else:
-        #print(valtok[tipusId][egysegId2:egysegId])
-        szorzo = 1
-        for e in valtok[tipusId][egysegId2:egysegId]:
-            szorzo *= e
-        eredmeny = szam*szorzo
-    print("#"*40)
+    if tipusId==5:
+        #Ha csak térfogatból térfogatba váltunk
+        if egysegId < 5 and egysegId2 < 5:
+            if egysegId<egysegId2:
+                #print(valtok[tipusId][egysegId:egysegId2])
+                szorzo = 1
+                for e in valtok[2][egysegId:egysegId2]:
+                    szorzo *= e
+                eredmeny = szam/szorzo
+            else:
+                #print(valtok[tipusId][egysegId2:egysegId])
+                szorzo = 1
+                for e in valtok[2][egysegId2:egysegId]:
+                    szorzo *= e
+                eredmeny = szam*szorzo
 
-    print("Eredmény:", szam, egysegek[tipusId][egysegId], "=", eredmeny, egysegek[tipusId][egysegId2])
+        #Ha csak űrmértékből űrmértékbe váltunk
+        if egysegId > 4 and egysegId2 > 4:
+            if egysegId<egysegId2:
+                szorzo = 1
+                for e in valtok[4][(egysegId-5):(egysegId2-5)]:
+                    print(e)
+                    szorzo *= e
+                eredmeny = szam/szorzo
+            else:
+                szorzo = 1
+                for e in valtok[4][(egysegId2-5):(egysegId-5)]:
+                    szorzo *= e
+                eredmeny = szam*szorzo
+        #Ha űrmértékből térfogatba vagy fordítva   
+        if egysegId < 5 or egysegId > 4 and egysegId2 < 5 or egysegId2 > 4:
+            #dm3-be váltás
+            if egysegId < 2:
+                szorzo = 1
+                for e in valtok[2][egysegId:2]:
+                    print("hello",e)
+                    szorzo *= e
+                szam2 = szam/szorzo
+            elif egysegId > 2 and egysegId < 5:
+                szorzo = 1
+                for e in valtok[2][egysegId:2]:
+                    szorzo *= e
+                szam2 = szam*szorzo
+                #Most már dm3-ből literbe át van váltva (1dm3=1l)
+            if egysegId2 < 8:
+                print("hello")
+                szorzo = 1
+                for e in valtok[4][(egysegId2-5):8]:
+                    szorzo *= e
+                eredmeny = szam2*szorzo
+            elif egysegId2 > 8:
+                szorzo = 1
+                for e in valtok[4][3]:
+                    szorzo *= e
+                eredmeny = szam/szorzo
+            #else:
+                #if egysegId2 < 8:
+                   # szorzo = 1
+                  #  for e in valtok[4][egysegId2:3]:
+                    #    szorzo *= e
+                   # eredmeny = szam*szorzo
+                #elif egysegId2 > 8:
+                   # szorzo = 1
+                   # for e in valtok[4][3:egysegId2]:
+                  #      szorzo *= e
+                  #  eredmeny = szam/szorzo
+        
+    print("#"*40)
+    if tipusId == 5:
+        print("Eredmény:", szam, terfogatUr[egysegId], "=", eredmeny, terfogatUr[egysegId2])
+    else:
+        print("Eredmény:", szam, egysegek[tipusId][egysegId], "=", eredmeny, egysegek[tipusId][egysegId2])
 
 
     #Házi: Csinosítás, számbekérésnél ellenőrzés hogy szám-e, while try, addig kell csinálni amig a tipusválasztónál a kilépést nem választjuk, (egészet egy while-ba)
