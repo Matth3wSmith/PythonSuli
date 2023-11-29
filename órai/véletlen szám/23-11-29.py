a5ee1fc1024d1eb6d2d0bc0de06dbf0bd0f5091c
Kovács Máté <kovacsm22@ADY-NAGYATAD.SULINET.HU>
2023-11-29 08:46:16 +0100

    Egyszerűbb változatot igényel

---
+++ órai/véletlen szám/23-11-29.py
@@ -0,0 +1,20 @@
+import random
+
+osztaly=[]
+osztalyAtlagok=[]
+for i in range(12):
+	tanulo=[]
+	for a in range(random.randrange(8,13)):
+		tanulo.append(random.randrange(1,6))
+	osztaly.append(tanulo)
+	#átlag összeadás
+	osszeg=0
+	for b,elem in enumerate(tanulo):
+		osszeg+=elem
+	atlag=osszeg/len(tanulo)
+	osztalyAtlagok.append(atlag)
+
+
+txt="Osztályátlagok {:.2f}"
+for k in osztalyAtlagok:
+	print(txt.format(k))
