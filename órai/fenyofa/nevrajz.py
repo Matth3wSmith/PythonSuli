dbf8466800aaf0b0bcc586a781470fa9f576f5f1
Máté <komate1011@gmail.com>
2024-01-28 22:04:06 +0100

    MÁTÉ név

--- órai/fenyofa/nevrajz.py
+++ órai/fenyofa/nevrajz.py
@@ -59,31 +59,47 @@ mate=[[0,0,
    300,0,
    0,0],
 
-   [0,0,
-   0,100,
-   100,100,
-   100,500,
+   ]
+a=[[0,400,
+   180,400,
+   250,250,
+   350,250,
+   420,400,
+   600,400,
+   400,0,
+   200,0,
+   0,400],
+
+   [250,200,
+    340,200,
+    370,50,
+    280,50,
+    250,200]]
+
+t=[0,0,
+   0,50,
+   175,50,
+   175,300,
+   225,300,
+   225,50,
+   400,50,
+   400,0,
+   0,0]
+
+e=[0,0,
    0,500,
-   0,600,
-   300,600,
-   300,500,
-   200,500,
-   200,100,
+   400,500,
+   400,400,
+   100,400,
+   100,300,
    400,300,
-   600,100,
-   600,500,
-   500,500,
-   500,600,
-   800,600,
-   800,500,
-   700,500,
-   700,100,
-   800,100,
-   800,0,
-   500,0,
    400,200,
-   300,0,
-   0,0]]
+   100,200,
+   100,100,
+   400,100,
+   400,0,
+   0,0
+   ]
 
 hatter="#ffffff"
 betuszinek=["red", hatter, "blue"]
@@ -95,21 +111,31 @@ mate2=[]
         #e=transzformaciok.forgat(e, 45)
         #mate2.append(e)
 
-print(mate2)
-mate2=transzformaciok.masol(mate)
-mate2=transzformaciok.nagyit(mate2,0.4)
-mate2=transzformaciok.eltolas(mate2,100,100)
-mate2=transzformaciok.forgat(mate2,-45)
+#mate2=transzformaciok.masol(mate)
+#mate2=transzformaciok.nagyit(mate2,0.4)
+#mate2=transzformaciok.eltolas(mate2,100,100)
 
-#for e in range(len(mate2)):
-        #if e%2==0:  
-                #canvas.create_line(transzformaciok.eltolas(mate2[e],225,225),width=5,fill="black")
-        #canvas.create_line(mate2[e],width=5,fill="black")
+mate=transzformaciok.nagyit(mate,0.4)
+mate=transzformaciok.eltolas(mate,100,100)
+canvas.create_line(mate,width=5,fill="black")
 
-while True:
-        canvas.delete("all")
-        mate2=transzformaciok.forgat(mate2,0.009)
-        for i,e in enumerate(mate2):
-                canvas.create_polygon(e,width=betuszinek[i],fill="blue")
-        win.update_idletasks()
-        win.update()
+a=transzformaciok.nagyit(a,0.6)
+a=transzformaciok.eltolas(a,450,100)
+canvas.create_line(a,width=5,fill="black")
+
+t=transzformaciok.nagyit(t,0.8)
+t=transzformaciok.eltolas(t,750,100)
+canvas.create_line(t,width=5,fill="black")
+
+e=transzformaciok.nagyit(e,0.5)
+e=transzformaciok.eltolas(e,1100,100)
+canvas.create_line(e,width=5,fill="black")
+
+#while True:
+        #canvas.delete("all")
+        #ate2=transzformaciok.forgat(mate2,0.009)
+        #for i,e in enumerate(mate2):
+                #canvas.create_polygon(e,width=betuszinek[i],fill="blue")
+        #win.update_idletasks()
+        #win.update()
+win.mainloop()
