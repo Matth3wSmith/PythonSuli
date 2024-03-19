bb0e7b1df3e7eb6e43a6af8e5bbae9dce91fa450
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-03-19 09:43:40 +0100

    NASA KÉSZ

--- órai/erettsegi/NASA/NASA.py
+++ órai/erettsegi/NASA/NASA.py
@@ -1,5 +1,6 @@
 #Gyártó: Kovács Máté
 from NASAclass import *
+import math
 
 f=open("NASAlog.txt","r")
 adatok=[]
@@ -19,4 +20,32 @@ print("5. feladat: Kérések száma: "+str(len(Keres.keresesek)))
 osszMeret=0
 for kereses in Keres.keresesek:
     osszMeret+=kereses.ByteMeret
-print("6. feladat: Válaszok összes mérete: "+str(osszMeret))
+
+#8. feladat
+keresSzam=len(Keres.keresesek)
+domainSzam=0
+
+for kereses in Keres.keresesek:
+    if kereses.Domain()=="Van":
+        domainSzam+=1
+arany=domainSzam/keresSzam*100
+
+#9. feladat
+allapotok=[0,0,0]
+"""for kereses in Keres.keresesek:
+    if kereses.allapotKod()=="200":
+        allapotok[0]+=1
+    elif kereses.allapotKod()=="404":
+        allapotok[1]+=1
+    elif kereses.allapotKod()=="304":
+        allapotok[2]+=1"""
+
+for egyAdat in Keres.keresesek:
+    egyAdat.allapotKodok()
+
+kodok=Keres.kodStat.keys()
+print("6. feladat: Válaszok összes mérete: {} byte".format(osszMeret))
+print("8. feladat: Domain-es kérések: {}%".format(round(arany,2)))
+print("9. feladat: Statisztika:")
+for kod in kodok:
+    print("\t{elso}: {masodik} db".format(elso=kod,masodik=Keres.kodStat[kod]))
--- órai/erettsegi/NASA/NASAclass.py
+++ órai/erettsegi/NASA/NASAclass.py
@@ -2,6 +2,8 @@
 
 class Keres():
     keresesek=[]
+    kodok=[]
+    kodStat={}
     cim=0
     datum=0
     kep=0
@@ -12,7 +14,6 @@ class Keres():
         self.datum=datum
         self.kep=kep
         self.allapot=allapot
-        
         #6. feladat
         meret=meret.strip("\n")
         if meret =="-":
@@ -22,11 +23,21 @@ class Keres():
 
         self.keresesek.append(self)
 
+        #9. feladat
+        self.kodStat[allapot]=0
+            
     #7. feladat
     def Domain(self):
-        if not Keres.keresesek[0].cim[-1].isnumeric():
+        
+        if self.cim[-1].isnumeric()==False:
             return "Van"
         else:
             return "Nincs"
         
-        
+        #return not self.cim[-1].isnumeric()
+    def allapotKodok(self):
+        keys = self.kodStat.keys()
+        for egyKey in keys:
+            if self.allapot==egyKey:
+                self.kodStat[egyKey]+=1
+        return self.kodStat
