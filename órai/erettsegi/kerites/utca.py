7bd397e7b42b74138ef81f070a59423d6926854f
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-04-25 13:16:27 +0200

    4. és 5. kész

--- órai/erettsegi/kerites/utca.py
+++ órai/erettsegi/kerites/utca.py
@@ -1,9 +1,13 @@
+import string
+import random
+abc = list(string.ascii_uppercase)
+
 class Telek:
     def __init__(self,sor) -> None:
-        vag=sor.split(" ")
+        vag=sor.strip().split(" ")
         self.oldal=int(vag[0])
         self.szelesseg=int(vag[1])
-        self.szin=vag[3]
+        self.szin=vag[2]
         self.hazszam=0
 
 telkek=[]
@@ -29,4 +33,39 @@ print("Az utolsó telek házszáma: {}".format(telkek[-1].hazszam))
 
 #4. feladat
 oldalSzerint=[[],[]]
+szomszedosSzin=[]
+for telek in telkek:
+    if telek.oldal==0:
+        oldalSzerint[0].append(telek)
+    else:
+        oldalSzerint[1].append(telek)
+
+for i,paratlanTelek in enumerate(oldalSzerint[1]):
+    if i+1<len(oldalSzerint[1]) and paratlanTelek.szin==oldalSzerint[1][i+1].szin:
+        if paratlanTelek.szin==":" or paratlanTelek.szin=="#":
+            pass
+        else:
+            szomszedosSzin.append(paratlanTelek.hazszam)
+                
+
+print("4. feladat")
+print("A szomszédossal egyezik a kerítés színe: {}".format(szomszedosSzin[0]))
+
+#5. feladat
+szomszedokSzine=[]
+kertHazszam=int(input("Add meg az eladott telek házszámát: "))
+for i,telek in enumerate(telkek):
+    if telek.hazszam==kertHazszam:
+        print("A kerítés színe / állapota: {}".format(telek.szin))
+        if i+1<len(telkek):
+            szomszedokSzine.append(telkek[i+1].szin)
+        if i-1>0:
+            szomszedokSzine.append(telkek[i-1].szin)
+        lehetseges=0
+        lehetseges=random.choice(abc)
+        while lehetseges in szomszedokSzine:
+            lehetseges=random.choice(abc)
+        print("Egy lehetséges festési szín: {}".format(lehetseges))
+
+
 
