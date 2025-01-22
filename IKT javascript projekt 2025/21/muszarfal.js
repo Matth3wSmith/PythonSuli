d3e7510bb4012858da0ef982d227dea1bbfc7ca7
Máté <komate1011@gmail.com>
2025-01-22 21:44:34 +0100

    valami

--- IKT javascript projekt 2025/21/muszarfal.js
+++ IKT javascript projekt 2025/21/muszarfal.js
@@ -1,6 +1,27 @@
-const c =document.getElementById("muszer")
-const ctx = c.getContext("2d");
+function onload(){
+    const c =document.getElementById("muszer")
+    const ctx = c.getContext("2d");
+    c.width=1000
+    c.height=600
+    sebessegMero(ctx)
 
-function korRajz(){
-    
+}
+function sebessegMero(ctx){
+    ctx.beginPath()
+    ctx.arc(400,200,100,0,2*Math.PI/20)
+    ctx.rotate(90*(Math.PI/180))
+    ctx.lineTo(100,100)
+
+
+    ctx.stroke()
+    ctx.closePath()
+
+}
+
+function korRajz(x,y,r){
+    ctx.beginPath()
+    ctx.arc(x,y,r,0,2*Math.PI)
+    ctx.stroke()
+    ctx.closePath()
+
 }
