593db0f9345d815d017edf4d059a6832112a530c
Máté <komate1011@gmail.com>
2025-01-23 22:14:13 +0100

    Sebességmérő

--- IKT javascript projekt 2025/21/muszarfal.js
+++ IKT javascript projekt 2025/21/muszarfal.js
@@ -1,4 +1,4 @@
-let szog = 1;
+let szog = 0;
 let ctx;
 function szogvaltoztatas(e){
     if (e.wheelDeltaY<0){
@@ -18,12 +18,12 @@ function onload(){
 	ctx = c.getContext("2d");
 	c.width=1000;
 	c.height=700;
-    c.addEventListener("wheel",szogvaltoztatas);
+    //c.addEventListener("wheel",szogvaltoztatas);
 
-	sebessegMero(ctx,szog);
+	sebessegMero(ctx);
 }
 
-function sebessegMero(ctx,szog){
+function sebessegMero(ctx){
 	let x = 200;
 	let y = 200;
 	let r = 100;
@@ -38,42 +38,70 @@ function sebessegMero(ctx,szog){
 	//r=√((x−u)**2+(y−v)**2)
     let a;
     let b;
-    console.log(szog)
-    if (szog<-90 || szog > 90){
-        if (szog > 360 || szog<-360){
-            szog=360-szog
+    
+    let a2;
+    let b2;
+
+    let sebesseg = 0;
+    for (let i = 11; i >= -2; i--){
+        let szog = i*20
+        console.log(szog)
+        if (szog<-90 || (szog > 90)){
+            if (szog > 360 || szog<-360){
+                szog=360-szog
+            }
+            szog = szog * (Math.PI/180);
+            b = y - Math.sin(szog)*r;
+            a = x + Math.sqrt(r**2-(y-b)**2);
+            
+            b2 = b + Math.sin(szog)*(1/6*r);
+            a2 = a - Math.sqrt((1/6*r)**2-(b2-b)**2);
         }
-        szog = szog * (Math.PI/180);
-        b = y - Math.sin(szog)*r;
-        a = x + Math.sqrt(r**2-(y-b)**2);
-    }
-    else{
-        szog = szog * (Math.PI/180);
-        b = y - Math.sin(szog)*r;
-        a = x - Math.sqrt(r**2-(y-b)**2);
-    }
+        else{
+            szog = szog * (Math.PI/180);
+            b = y - Math.sin(szog)*r;
+            a = x - Math.sqrt(r**2-(y-b)**2);
+            
+            b2 = b + Math.sin(szog)*(1/6*r);
+            a2 = a + Math.sqrt((1/6*r)**2-(b2-b)**2);
+        }
+        ctx.beginPath();
+        ctx.moveTo(a2+r,b2);
+        ctx.lineTo(a+r,b);
+        ctx.stroke();
+        ctx.closePath();
 
-    /*if (szog > 90 && szog <= 180){
-
-        szog = szog * (Math.PI/180);
-        b = y - Math.sin(szog)*r;
-        a = x + Math.sqrt(r**2-(y-b)**2);
-    }
-    else if (szog>180 && szog<=270) {
-        szog = szog * (Math.PI/180);
-        b = y + Math.sin(szog)*r;
-        a = x + Math.sqrt(r**2-(y-b)**2);
+        //
+        ctx.beginPath();
+        ctx.moveTo(x+r,y);
+        ctx.lineTo(a+r,b);
+        ctx.stroke();
+        ctx.closePath();
+    }/*
+    for (let i = 11; i >= -2; i--){
+        let szog = i*20
+        console.log(szog)
+        if (szog<-90 || (szog > 90)){
+            if (szog > 360 || szog<-360){
+                szog=360-szog
+            }
+            szog = szog * (Math.PI/180);
+            b = y - Math.sin(szog)*r;
+            a = x + Math.sqrt(r**2-(y-b)**2);
+            
+        }
+        else{
+            szog = szog * (Math.PI/180);
+            b = y - Math.sin(szog)*r;
+            a = x - Math.sqrt(r**2-(y-b)**2);
+            
+        }
+        ctx.beginPath();
+        ctx.moveTo(x+r,y);
+        ctx.lineTo(a+r,b);
+        ctx.stroke();
+        ctx.closePath();
     }*/
-
-/*
-	ctx.beginPath()
-	ctx.fillStyle="black"
-	ctx.arc(x+r,y,1,0,Math.PI*2)
-	ctx.stroke()*/
-	
-	ctx.moveTo(x+r,y);
-	ctx.lineTo(a+r,b);
-	ctx.stroke();
 
 
 	
