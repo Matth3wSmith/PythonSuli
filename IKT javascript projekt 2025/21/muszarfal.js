8ecbef33689db54c3ad6e720ba454b406684f482
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2025-01-24 09:40:46 +0100

    14-es

--- IKT javascript projekt 2025/21/muszarfal.js
+++ IKT javascript projekt 2025/21/muszarfal.js
@@ -7,9 +7,12 @@ function szogvaltoztatas(e){
     else{
         szog+=5;
     }
-    ctx.clearRect(0,0,1000,700)
+    torles(ctx)
+	sebessegMero(ctx);
+}
 
-	sebessegMero(ctx,szog);
+function torles(ctx){
+    ctx.clearRect(0,0,1000,700)
 }
 
 function onload(){
@@ -22,12 +25,19 @@ function onload(){
 
 	sebessegMero(ctx);
 }
-
-function sebessegMero(ctx){
+let sebesseg = 0
+function sebessegNov(){
+    sebesseg+=20
+    if (sebesseg<=260){
+        torles(ctx)
+        sebessegMero(ctx,sebesseg,20)
+    }
+}
+function sebessegMero(ctx,sebesseg=0,noveles=0){
+    //Sebességmérő óra x,y koordinátája és sugrának beállítása
 	let x = 200;
 	let y = 200;
 	let r = 100;
-	let kezdoSzog=20*(Math.PI/180);
 
 	//Körvonal
 	ctx.beginPath();
@@ -35,15 +45,15 @@ function sebessegMero(ctx){
 	ctx.stroke();
 	ctx.closePath();
 
-	//r=√((x−u)**2+(y−v)**2)
+    //Beosztás körvonalon lévő pontjának koordinátája
     let a;
     let b;
-    
+    //Beosztás körben lévő pontjának koordinátája
     let a2;
     let b2;
 
-    let sebesseg = 0;
-    for (let i = 11; i >= -2; i--){
+    //A Sebességmérő óra beosztásai, -2-től rajzolj (-40 fok) 11-ig (220 fok)
+    for (let i = -2; i <= 11; i++){
         let szog = i*20
         console.log(szog)
         if (szog<-90 || (szog > 90)){
@@ -71,16 +81,39 @@ function sebessegMero(ctx){
         ctx.stroke();
         ctx.closePath();
 
-        //
-        ctx.beginPath();
-        ctx.moveTo(x+r,y);
-        ctx.lineTo(a+r,b);
-        ctx.stroke();
-        ctx.closePath();
-    }/*
-    for (let i = 11; i >= -2; i--){
-        let szog = i*20
-        console.log(szog)
+    }
+    
+    //Sebességmérő óra mutató
+    /*let sebessegSzog = sebesseg-40;
+
+    if (sebessegSzog<-90 || (sebessegSzog > 90)){
+        if (sebessegSzog > 360 || sebessegSzog<-360){
+            sebessegSzog=360-sebessegSzog
+        }
+        sebessegSzog = sebessegSzog * (Math.PI/180);
+        b = y - Math.sin(sebessegSzog)*r;
+        a = x + Math.sqrt(r**2-(y-b)**2);
+    }
+    else{
+        sebessegSzog = sebessegSzog * (Math.PI/180);
+        b = y - Math.sin(sebessegSzog)*r;
+        a = x - Math.sqrt(r**2-(y-b)**2);
+        
+    }
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.moveTo(x+r,y);
+    ctx.lineTo(a+r,b);
+    ctx.stroke();
+    ctx.lineWidth = 1;
+    ctx.closePath();*/
+
+
+    
+    let i =0;
+    setInterval(function() {
+        let szog = i+sebesseg-noveles-40;
+        console.log(szog);
         if (szog<-90 || (szog > 90)){
             if (szog > 360 || szog<-360){
                 szog=360-szog
@@ -95,14 +128,17 @@ function sebessegMero(ctx){
             b = y - Math.sin(szog)*r;
             a = x - Math.sqrt(r**2-(y-b)**2);
             
-        }
+        
         ctx.beginPath();
         ctx.moveTo(x+r,y);
         ctx.lineTo(a+r,b);
         ctx.stroke();
         ctx.closePath();
-    }*/
-
+        i++;
+        torles(ctx)
+    }
+    },1000)
+        
 
 	
 
