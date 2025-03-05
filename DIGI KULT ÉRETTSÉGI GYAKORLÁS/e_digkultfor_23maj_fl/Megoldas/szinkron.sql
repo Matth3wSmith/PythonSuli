d1a91487501fd9838f21bf744ea8e50cbd08c44f
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2025-03-05 14:05:23 +0100

    Szinkron feladat

--- DIGI KULT ÉRETTSÉGI GYAKORLÁS/e_digkultfor_23maj_fl/Megoldas/szinkron.sql
+++ DIGI KULT ÉRETTSÉGI GYAKORLÁS/e_digkultfor_23maj_fl/Megoldas/szinkron.sql
@@ -2,3 +2,22 @@
     SELECT cim, eredeti FROM film  WHERE magyarszoveg="Heltai Olga";
 
 --- 3. feladat --- 3szinkrend ---
+
+
+
+
+--- 8. feladat --- 8rendszin ---
+    SELECT DISTINCT rendezo AS "Színész-rendező" FROM film AS folekerdezes WHERE rendezo IN(SELECT szinesz FROM szinkron WHERE szinesz=folekerdezes.rendezo);
+
+--- 9. feladat --- 9pap ---
+    SELECT cim, hang 
+        FROM szinkron 
+            JOIN film ON film.filmaz=szinkron.filmaz 
+        WHERE szinkron.filmaz IN
+            (SELECT film.filmaz 
+                FROM film JOIN szinkron ON film.filmaz=szinkron.filmaz 
+                WHERE hang="Pap Kati") 
+            AND hang != "Pap Kati" 
+        ORDER BY 1, 2;
+
+
